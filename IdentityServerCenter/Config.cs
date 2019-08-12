c2eb068c223a7fdc110c8386d29e354928c70cae
lxg <1436215718@qq.com>
2019-08-12 17:10:18 +0800

    添加项目文件。

---
+++ IdentityServerCenter/Config.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace IdentityServerCenter
+{
+    public static class Config
+    {
+        //api地址
+        public static IEnumerable<ApiResource> GetResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api","My Api")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId="client",
+                    AllowedGrantTypes={ GrantType.ClientCredentials},
+                    ClientSecrets ={new Secret("secret".Sha256())},
+                    AllowedScopes={ "api" }//ApiResource
+                }
+            };
+        }
+    }
+}
---
+++ IdentityServerCenter/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using IdentityServer4;
+
+namespace IdentityServerCenter
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //IdentityServer4
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetResources())
+                .AddInMemoryClients(Config.GetClients());//http://localhost:5000/.well-known/openid-configuration
+
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            //app.UseMvc();
+            app.UseIdentityServer();
+        }
+    }
+}
